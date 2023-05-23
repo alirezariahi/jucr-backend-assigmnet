3c0720c70074ecc285762c17c1a338ae9cd9f284
Alireza Riahi <ali.r.riahi@gmail.com>
2023-05-23 12:46:46 +0200

    update

--- apps/station/test/app.e2e-spec.ts
+++ apps/station/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { StationModule } from './../src/station.module';
 
 describe('StationController (e2e)', () => {
